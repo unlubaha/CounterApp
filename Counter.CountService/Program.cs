35623ec4c595dd8ff23d4e5546adf0c79dc99969
BAHA <BAHA@DESKTOP-6TB0GCH>
2024-10-29 17:20:35 +0300

    #Postgre migrations created, its works for different db or same db

--- Counter.CountService/Program.cs
+++ Counter.CountService/Program.cs
@@ -1,15 +1,24 @@
+using Counter.CountService.Services;
+using Counter.Entities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//if you need different database for every service
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"),
+//        b => b.MigrationsAssembly("Counter.CountService")));
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql());
+
+builder.Services.AddScoped<ICountService, CountService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
